b82cdeb22269a0124ae36ffb705a82e1a540a429
oscarmtzo <icehman@hotmail.com>
2021-07-20 17:59:10 -0500

    Iteration3: Total | done

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -12,15 +12,26 @@ function updateSubtot($product) {
   // qty = parseFloat(qty.value) //=> 0 typeof qty => number
   sub_total.innerText = priceUnit*parseFloat(qty.value)
   // qty.addEventListener('input')
+  return parseFloat(sub_total.innerText)
+}
+
+function updateTotal( total ){
+  let h2Total = document.querySelector('h2 span') // => <span>0</span>
+  // console.log(h2Total.innerText)// => 0
+  h2Total.innerText = total
 }
 
 function calcAll() {
   // Iteration 1.2
   // updateSubtot($cart)
+  let sum = 0
   for (product of $table){
-    updateSubtot(product)
-    console.log(product)
+    sum +=  updateSubtot(product)
+   
+    //console.log(product) //<tr class="product">...</tr>
   }
+  // console.log(sum)
+  updateTotal(sum)
   // for( let i = 0; i<$table.length; i++ ){
   //   console.log($table[i])
   //   updateSubtot($table[i])
