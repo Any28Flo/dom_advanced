7688e6fc1d47940945853f084c412d22843f52a4
SamuelLopezC <samuellopez.dev@gmail.com>
2021-07-20 14:30:43 -0500

    MAKE | Aqui esta la primera iteracion (solo suma, no resta)

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,11 +1,22 @@
 let $cart = document.querySelector('#cart tbody');
 let $calc = document.getElementById('calc');
+let $price = document.querySelector('.pu').querySelector('span').textContent;
+let $subtotl = document.querySelector('.subtot').querySelector('span');
+let $quanty = document.querySelector('.qty').querySelector('input');
+console.log($quanty.textContent);
+let $total = document.querySelector('h2').querySelector('span');
+console.log($total);
 
-function updateSubtot($product) {
-  // Iteration 1.1
+//Esta variable recoge el valor de quantity en cada click dado con el evento de debajo
+let cantidad = 0; 
+$quanty.addEventListener('click',updateSubtot);
+
+function updateSubtot() {
+  cantidad ++;
+  $subtotl.innerHTML = Number($price) * cantidad;
 }
 
 function calcAll() {
-  // Iteration 1.2
+  $total.innerHTML = $subtotl.textContent;
 }
 $calc.onclick = calcAll;
