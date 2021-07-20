cc94ceef63e6f9047defc0a637d84734390cfd3a
oscarmtzo <icehman@hotmail.com>
2021-07-20 17:12:51 -0500

    Iteraccion 2 | completa

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,16 +1,13 @@
 let $cart = document.querySelector('#cart tbody');
 let $calc = document.getElementById('calc');
-// let $table = document.querySelectorAll('.product')
+let $table = document.querySelectorAll('.product')
 // $table => NodeList(2) [tr.product, tr.product]
 
 function updateSubtot($product) {
   // Iteration 1.1
-  // let priceUnit = $product.querySelector('.pu span') //=>span
-  // let qty = $product.querySelector('.qty input') //=>input
-  // let sub_total = $product.querySelector('.subtot span')
-  let priceUnit = document.querySelector('.pu span') //=>span
-  let qty = document.querySelector('.qty input') //=>input
-  let sub_total = document.querySelector('.subtot span')
+  let priceUnit = $product.querySelector('.pu span') //=>span
+  let qty = $product.querySelector('.qty input') //=>input
+  let sub_total = $product.querySelector('.subtot span')
   priceUnit = parseFloat(priceUnit.innerHTML)
   // qty = parseFloat(qty.value) //=> 0 typeof qty => number
   sub_total.innerText = priceUnit*parseFloat(qty.value)
@@ -19,11 +16,11 @@ function updateSubtot($product) {
 
 function calcAll() {
   // Iteration 1.2
-  updateSubtot($cart)
-  // for (product of $table){
-  //   updateSubtot(product)
-  //   console.log(product)
-  // }
+  // updateSubtot($cart)
+  for (product of $table){
+    updateSubtot(product)
+    console.log(product)
+  }
   // for( let i = 0; i<$table.length; i++ ){
   //   console.log($table[i])
   //   updateSubtot($table[i])
