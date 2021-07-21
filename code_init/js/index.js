fbc845e09f6438e292b61a73ff23503d51fb110d
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-20 19:38:19 -0500

    Feat | Iteracion 1

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -1,11 +1,20 @@
 let $cart = document.querySelector('#cart tbody');
 let $calc = document.getElementById('calc');
 
+
 function updateSubtot($product) {
   // Iteration 1.1
+  let quatity=$product.querySelector('.qty input');
+  let priceUnit=$product.querySelector('.pu span');
+  let subTotal =$product.querySelector('.subtot span');
+  let resultado=parseInt(quatity.value)*parseFloat(priceUnit.textContent);
+  subTotal.innerHTML =resultado;
+  return resultado;
 }
 
 function calcAll() {
   // Iteration 1.2
+  updateSubtot($cart);
 }
+
 $calc.onclick = calcAll;
