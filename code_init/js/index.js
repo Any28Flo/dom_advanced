503abf65b765656811e9df0ea0af0f1a57aec311
ISRAEL HERNANDEZ VAZQUEZ <al2183045545@azc.uam.mx>
2021-07-20 20:13:28 -0500

    Feat | Iteracion 2: Agregar otro producto

--- code_init/js/index.js
+++ code_init/js/index.js
@@ -7,14 +7,24 @@ function updateSubtot($product) {
   let quatity=$product.querySelector('.qty input');
   let priceUnit=$product.querySelector('.pu span');
   let subTotal =$product.querySelector('.subtot span');
+  console.log(priceUnit.textContent);
+  console.log(quatity.value);
+  console.log(subTotal.textContent);
   let resultado=parseInt(quatity.value)*parseFloat(priceUnit.textContent);
+  console.log(resultado);
+
   subTotal.innerHTML =resultado;
   return resultado;
 }
 
 function calcAll() {
   // Iteration 1.2
-  updateSubtot($cart);
+  let products=$cart.getElementsByClassName('product');
+  console.log(products);
+  for(let i=0;i<products.length;i++){
+    products[i]=updateSubtot(products[i]);
+  }
+  
 }
 
 $calc.onclick = calcAll;
